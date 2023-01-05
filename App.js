97264edb63587c9bb77c5ffeab626305b4166e02
IvanSekulic <ivansekulic4@gmail.com>
2023-01-05 16:25:00 +0100

    style for task feature added

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     {/* Today's tasks */}
+     <View style={styles.taskWrapper}>
+      <Text style={styles.sectionTitle}>Today's tasks</Text>
+      <View style={styles.items}>
+        {/* This will tasks go */}
+        <Task text = {'task 1'}/>
+        <Task text = {'task 2'}/>
+        <Task text = {'task 3'}/>
+      </View>
+
+     </View>
+
     </View>
   );
 }
@@ -13,8 +24,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+    
+  },
+  taskWrapper:{
+    paddingTop:80,
+    paddingHorizontal:20,
+  },
+  sectionTitle:{
+    fontSize:24,
+    fontWeight:'bold'
+  },
+  items:{
+    marginTop: 30, 
   },
 });
